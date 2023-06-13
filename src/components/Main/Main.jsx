ae6049d29f248c207e076ae347a9d4428a52c681
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-06-13 19:40:37 +0300

    feat: add new section for items in main layout

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import MainCSS from './Main.module.css';
+import Item from '../Item/Item';
+
+import data from '../../data/data.json';
 
 const Main = () => {
   return (
     <>
-      <p className={MainCSS.intro}>
-        The fastest brunch delivery in
-        <span className={MainCSS.city}> Your city</span>
-      </p>
+      <section className={MainCSS.introSection}>
+        <p className={MainCSS.introHeading}>
+          The fastest brunch delivery in
+          <span className={MainCSS.city}> Your city</span>
+        </p>
 
-      <div>
         <p className={MainCSS.desc}>
           This is my Exploration for Fruit - Food Delivery Landing Page 🍕. How
           about you
         </p>
-      </div>
+      </section>
+
+      <section className={MainCSS.mediaScroll}>
+        {data.items.map((item) => {
+          return (
+            <Item
+              name={item.name}
+              caption={item.caption}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </section>
     </>
   );
 };
