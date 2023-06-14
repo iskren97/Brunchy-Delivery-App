cdf9fa157b859d084d1eee36ad8ae06aa2426444
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-06-14 14:24:48 +0300

    feat: add functions for increasing/getting item amount in basket

--- src/context/basketContext.js
+++ src/context/basketContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from 'react';
+import data from '../data/data.json';
 
 const BasketContext = createContext({});
 
@@ -8,6 +9,7 @@ export const useBasket = () => {
 
 export const BasketProvider = ({ children }) => {
   const [basketAmount, setBasketAmount] = useState(0);
+  const [basketItems, setBasketItems] = useState([]);
 
   const increaseBasketAmount = (price) => {
     setBasketAmount((currAmount) => (currAmount += Number(price)));
@@ -15,9 +17,43 @@ export const BasketProvider = ({ children }) => {
 
   const getBasketTotalAmount = () => basketAmount.toFixed(2);
 
+  const increaseBasketQuantity = (id) => {
+    setBasketItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+
+  const getBasketItems = () => {
+    return data.items
+      .map((item) => {
+        return basketItems.map((basketItem) =>
+          item.id === basketItem.id
+            ? { ...item, quantity: basketItem.quantity }
+            : null
+        );
+      })
+      .flat()
+      .filter((item) => item);
+  };
+
   return (
     <BasketContext.Provider
-      value={{ getBasketTotalAmount, increaseBasketAmount }}
+      value={{
+        getBasketTotalAmount,
+        increaseBasketAmount,
+        increaseBasketQuantity,
+        getBasketItems,
+      }}
     >
       {children}
     </BasketContext.Provider>
