c6a4cad559b6ee2c9fb1ac39a698475c7ad1086e
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-06-14 14:29:25 +0300

    feat: present items in basket review component

--- src/components/OrderReview/OrderReview.jsx
+++ src/components/OrderReview/OrderReview.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OrderReviewCSS from './OrderReview.module.css';
 import ItemCSS from '../Item/Item.module.css';
+import { useBasket } from '../../context/basketContext';
 
 const OrderReview = ({ totalAmount }) => {
+  const { getBasketItems } = useBasket();
+  const [basketItems, setBasketItems] = useState([]);
+
+  useEffect(() => {
+    setBasketItems(getBasketItems());
+  }, []);
+
   return (
     <div className={OrderReviewCSS.container}>
-      <div className={OrderReviewCSS.innerContainer}>
-        <p>Test</p>
+      {basketItems.map((basketItem) => {
+        return (
+          <div className={OrderReviewCSS.innerContainer}>
+            <p>
+              {basketItem.name} - {basketItem.quantity}
+            </p>
 
-        <p>
-          <span className={ItemCSS.dollarSign}>$ </span>
-          Price
-        </p>
-      </div>
+            <p>
+              <span className={ItemCSS.dollarSign}>$ </span>
+              {(basketItem.price * basketItem.quantity).toFixed(2)}
+            </p>
+          </div>
+        );
+      })}
 
       <hr className={OrderReviewCSS.divider} />
 
