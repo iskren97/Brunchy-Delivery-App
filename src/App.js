74f64718a0fc4e1815e2e01ef0e747dd7ee13406
Iskren Gyorev <iskren.gyorev@mentormate.com>
2023-06-14 18:01:15 +0300

    feat: refactor code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import { BasketProvider } from './context/basketContext';
 
 const App = () => {
   return (
     <BasketProvider>
-      <Header />
       <Main />
     </BasketProvider>
   );
